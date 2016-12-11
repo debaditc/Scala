e6fdd30f86ed195a6c917d28fdb7b84ff7cfafdc
Debaditya Chakravorty <debaditya.chakravorty@gmail.com>
2016-12-11 02:27:57 -0500

    Update jsonParse.scala

--- jsonParse.scala
+++ jsonParse.scala
@@ -1,19 +1,16 @@
 import scala.util.parsing.json.JSON._
 
-
 object jsonParse {
-
   def main (args : Array[String])
   {
     class CC[T] { def unapply(a:Any):Option[T] = Some(a.asInstanceOf[T]) }
-  
     object M extends CC[Map[String, Any]]
     object L extends CC[List[Any]]
     object S extends CC[String]
     object D extends CC[Double]
     object B extends CC[Boolean]
     
-  val jsonString =
+   val jsonString =
       """
         |{
         | "languages": [{
@@ -28,16 +25,18 @@ object jsonParse {
         |}
       """.stripMargin
   
-          val result = for {
-        Some(M(map)) <- List(scala.util.parsing.json.JSON.parseFull(jsonString))
-        L(languages) = map("languages")
-        M(language) <- languages
-        S(name) = language("name")
-        B(active) = language("is_active")
-        D(completeness) = language("completeness")
-    } yield {
-        (name, active, completeness)
-    }
+        val result = for 
+        {
+          Some(M(map)) <- List(scala.util.parsing.json.JSON.parseFull(jsonString))
+          L(languages) = map("languages")
+          M(language) <- languages
+          S(name) = language("name")
+          B(active) = language("is_active")
+          D(completeness) = language("completeness")
+        } yield 
+        {
+          (name, active, completeness)
+        }
     
     println("Result - " + result)
     
